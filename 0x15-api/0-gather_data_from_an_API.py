dcf463a9e8f81bf91ecf005d1fdaab121b947b44
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-05-26 19:56:28 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,39 +3,41 @@
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     """
     Fetch and display the TODO list progress for a given employee ID.
-    
-    This function retrieves the employee's name and their TODO list from the JSONPlaceholder API.
-    It then calculates the number of completed tasks and the total number of tasks, and prints this information.
+    This function retrieves the employee's name and their TO
+    It then calculates the number of completed tasks and the total
     Additionally, it lists the titles of the completed tasks.
     
     Args:
-        employee_id (int): The ID of the employee whose TODO list progress is to be fetched.
+        employee_id (int): The ID of the employee whose TODO list
     
     Raises:
-        requests.exceptions.RequestException: If there is an issue with the HTTP request.
+        requests.exceptions.RequestException: If there 
     """
     try:
         # Fetch employee details
-        user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+        user = 'https://jsonplaceholder.typicode.com/users/'
+        user_response = requests.get(f'{user}{employee_id}')
         user_response.raise_for_status()
         user_data = user_response.json()
         employee_name = user_data.get('name')
 
         # Fetch employee's todos
-        todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+        todo = 'https://jsonplaceholder.typicode.com/todos?userId='
+        todos_response = requests.get(f'{todo}{employee_id}')
         todos_response.raise_for_status()
         todos_data = todos_response.json()
 
         # Calculate the number of completed and total tasks
-        total_tasks = len(todos_data)
+        tt = len(todos_data)
         done_tasks = [todo['title'] for todo in todos_data if todo['completed']]
-        number_of_done_tasks = len(done_tasks)
+        ndt = len(done_tasks)
 
         # Display the results
-        print(f"Employee {employee_name} is done with tasks({number_of_done_tasks}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with tasks({ndt}/{tt}):")
         for task in done_tasks:
             print(f"\t {task}")
 
@@ -46,7 +48,7 @@ if __name__ == "__main__":
     """
     Main entry point of the script.
     
-    This block checks if the correct number of arguments is provided, parses the employee ID,
+    This block checks if the correct number of
     and calls the get_employee_todo_progress function.
     """
     if len(sys.argv) != 2:
